2795cc8d9a998d2b36dc21b5cc73dd8d4d248693
Gibbs <mr.darmstadtiumgmail.com>
2024-09-16 15:53:26 +0200

    Create Book model with title, author, cover inventory and daily_fee fields

--- books_service/models.py
+++ books_service/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class BookCover(models.TextChoices):
+        hard = "HARD"
+        soft = "SOFT"
+
+    Title = models.CharField(max_length=100)
+    Author = models.CharField(max_length=100)
+    Cover = models.TextField(choices=BookCover)
+    Inventory = models.PositiveIntegerField()
+    Daily_fee = models.DecimalField()
