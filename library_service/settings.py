27f9502488b1c6f40dcaae2afef14b4debce9d65
Gibbs <mr.darmstadtiumgmail.com>
2024-09-27 10:35:44 +0200

    Add permission functionality. Only admin can create/update/delete books. All users can get list of all books. JWT authentication is used

--- library_service/settings.py
+++ library_service/settings.py
@@ -139,6 +139,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "library_service.permissions.IsAdminOrIfAuthenticatedReadOnly",
+    ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
